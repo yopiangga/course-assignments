d01e528ada5ede685a9ab48c7879ddd2b7411a25
@yopigambyok <yopigambyok@gmail.com>
2021-07-24 11:30:23 +0700

    compoent 2

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent'
+import { DISHES } from './shared/dishes'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Alfian Prisma Yopiangga</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Alfian Prisma Yopiangga</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
